fffe8a6ffd524713f7bdebcf070dc367d269d1e7
inkSpace-Abhi <67835685+inkSpace-Abhi@users.noreply.github.com>
2024-05-19 22:26:36 +0530

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,1 +1,3 @@
 echo hi
+osascript -e 'tell application "System Events" to set picture of every desktop to POSIX file "/Users/abhiram/SysReq/Wallpapers/antoine-le-idiwVxHqmGg-unsplash.jpg"'
+osascript -e 'tell application "System Events" to set picture of every desktop to POSIX file "~/SysReq/Wallpapers/antoine-le-idiwVxHqmGg-unsplash.jpg"'
